0e7d4f98cc7cb780c8cfc39d1e584375b3dbc6b4
Chris Arderne <chris@rdrn.me>
2022-08-11 17:38:52 -0400

    Add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,10 @@
-from sigexport.main import source_location
+from sigexport.main import source_location, timestamp_format
 
 
 def test_source_location():
     source_location()
+
+
+def test_timestamp_format():
+    res = timestamp_format(76823746823)
+    assert res == "1972-06-07 23:55"
